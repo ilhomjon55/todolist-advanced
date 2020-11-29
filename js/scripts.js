36923605279728e0bdb66d5cbdfcdf2767804941
ilhomjon55 <ilhomshamidinov55@gmail.com>
2020-11-29 12:56:45 +0500

    Indent with beautify

--- js/scripts.js
+++ js/scripts.js
@@ -5,75 +5,80 @@ var elTasksList = $_('.js-tasks__list')
 var elTasksleft = $_('.js-tasks__left')
 var elTaskTemplate = $_('#todo__item-template').content
 
+
 // Create empty array for tasks
 var tasksArr = []
 
-// Create Tasks item
+
+// Create Tasks item ****************************
 var createTasksItem = (arr) => {
-	elTask = elTaskTemplate.cloneNode(true)
+   elTask = elTaskTemplate.cloneNode(true)
 
-	$_('.tasks__text', elTask).textContent = arr.taskText
-	$_('.js-remove-task-btn', elTask).dataset.Id = arr.id
+   $_('.tasks__text', elTask).textContent = arr.taskText
+   $_('.js-remove-task-btn', elTask).dataset.Id = arr.id
 
-	return elTask
+   return elTask
 }
 
-// Render tasks to document
+
+// Render tasks to document ***********************
 var renderTasks = (arr) => {
-	elTasksList.innerHTML = ''
+   elTasksList.innerHTML = ''
 
-	var elTasksFragment = document.createDocumentFragment()
+   var elTasksFragment = document.createDocumentFragment()
 
-	arr.forEach((task) => {
-		elTasksFragment.append(createTasksItem(task))
-	})
+   arr.forEach((task) => {
+      elTasksFragment.append(createTasksItem(task))
+   })
 
-	elTasksList.append(elTasksFragment)
+   elTasksList.append(elTasksFragment)
 }
 
-// Listen submit of elFormTask
+
+// Listen submit of elFormTask ======================
 elFormTask.addEventListener('submit', (evt) => {
-	evt.preventDefault()
+   evt.preventDefault()
 
-	// Get value of inputTask
-	var inputTask = elInputTask.value.trim()
+   // Get value of inputTask
+   var inputTask = elInputTask.value.trim()
 
-	// Prevent empty value
-	if (!inputTask) {
-		alert('Please, enter text!')
-		return
-	}
+   // Prevent empty value
+   if (!inputTask) {
+      alert('Please, enter text!')
+      return
+   }
 
-	// Create and push task object
-	tasksArr.push({
-		id: 1,
-		taskText: inputTask,
-		completed: false
-	})
+   // Create and push task object
+   tasksArr.push({
+      id: 1,
+      taskText: inputTask,
+      completed: false
+   })
 
-	// Add id to task object
-	tasksArr.forEach((task, index) => {
-		task.id = index + 1
-	})
+   // Add id to task object
+   tasksArr.forEach((task, index) => {
+      task.id = index + 1
+   })
 
-	renderTasks(tasksArr)
+   renderTasks(tasksArr)
 
-	// Little UI feature
-	elInputTask.value = ''
-	elInputTask.focus()
+   // Little UI feature
+   elInputTask.value = ''
+   elInputTask.focus()
 })
 
-// Listen click of elTasksList to delete task
+
+// Listen click of elTasksList to delete task ================
 elTasksList.addEventListener('click', (evt) => {
-	if (evt.target.matches('.js-remove-task-btn')) {
-		// Delete element
-		evt.target.closest('li').remove()
+   if (evt.target.matches('.js-remove-task-btn')) {
+      // Delete element
+      evt.target.closest('li').remove()
 
-		// Find element form array and splice
-		var foundTaskIndex = tasksArr.findIndex((task) => {
-			return task.id === evt.target.dataset.id
-		})
+      // Find element form array and splice
+      var foundTaskIndex = tasksArr.findIndex((task) => {
+         return task.id === evt.target.dataset.id
+      })
 
-		tasksArr.splice(foundTaskIndex, 1)
-	}
+      tasksArr.splice(foundTaskIndex, 1)
+   }
 })
